a4da9d663ef8e479a10ac9764e34ab64f5067026
im james <168637111+Artcrypto01@users.noreply.github.com>
2024-05-26 03:25:07 +0700

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,42 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/nAhiCHKvZkhkp4A7PkkCIBON0-BXW26d`,
+      //accounts: [process.env.privateKey]
+    },
+    matic: {
+      url: "https://polygon-mainnet.g.alchemy.com/v2/nAhiCHKvZkhkp4A7PkkCIBON0-BXW26d",
+      //accounts: [process.env.privateKey]
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/1PlaDNQ58szySx4MDYdRwGM3b07Na2iX",
+      accounts: [ "dd9ca7182deefc74e6d786b324763494316050b5c61a9d9cb8d65e700421e6fd" ]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
